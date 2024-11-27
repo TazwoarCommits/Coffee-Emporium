2debd018989a752ede4b3531ccfd44a25c8ce1a4
TazwoarCommits <tazwoarlives@gmail.com>
2024-11-27 23:25:42 +0600

    third

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <h1 className='text-4xl text-center text-amber-900'>Coffee Emporium</h1>
       <div className="my-8">
            <div className="text-center">
-            <h3 className="text-2xl">Available Coffees</h3>
+            <h3 className="text-2xl mb-6">Available Coffees</h3>
            </div>
            <div className="w-11/12 mx-auto grid md:grid-cols-2 gap-6">
             {
--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -2,24 +2,24 @@
 const CoffeeCard = ({ coffee }) => {
     const { photo, name, taste, category } = coffee;
     return (
-        <div className="card card-side bg-base-100 shadow-xl">
-            <figure>
-                <img
-                    src={photo}
-                    alt="Coffee" />
-            </figure>
-            <div className="flex justify-between border-4">
+        <div className="flex justify-around items-center mx-8 border-2 border-amber-600">
+            <div className="flex justify-between items-center bg-base-100 shadow-xl">
+                <figure>
+                    <img
+                        src={photo}
+                        alt="Coffee" />
+                </figure>
                 <div>
                     <h2 className="card-title">{name}</h2>
                     <p>{category}</p>
                     <p>{taste}</p>
                 </div>
-                <div>
-                    <div className="join join-vertical">
-                        <button className="btn join-item">View</button>
-                        <button className="btn join-item">Edit</button>
-                        <button className="btn join-item">Delete</button>
-                    </div>
+            </div>
+            <div>
+                <div className="join join-vertical">
+                    <button className="btn join-item">View</button>
+                    <button className="btn join-item">Edit</button>
+                    <button className="btn join-item">Delete</button>
                 </div>
             </div>
         </div>
